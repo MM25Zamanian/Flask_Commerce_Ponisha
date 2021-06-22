c06de475646a28cc5ccc3a44c8db953d50fe0de0
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2021-06-22 21:01:06 +0430

    Edit app.py

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
+from database import (
+    SELECT_WHERE
+)
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def index():
+    return render_template('index.html')
+
+
+@app.route('/categories/<category>')
+def category(category):
+    q = SELECT_WHERE(
+        'products', [
+            'id',
+            'name',
+            'category',
+            'description',
+        ],
+        'category',
+        category
+    )
+    return render_template('index.html', category=q)
